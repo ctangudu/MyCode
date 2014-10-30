8a8b8705f41d457d7b8ffff167038e85cba4f6b0
Chaitanya Tangudu <chaitanya.tangudu@gmail.com>
2014-10-30 15:39:53 -0500

    test

--- MyJavaProj/src/my/java/MyFirstClass.java
+++ MyJavaProj/src/my/java/MyFirstClass.java
@@ -4,4 +4,12 @@ public class MyFirstClass {
 	public static void main(){
 		System.out.println("Yahoo my first class");
 	}
+	
+	class Parent{
+		void parentMethod(){
+			System.out.println("Parent method");
+		}
+	} 
+	
 }
+
