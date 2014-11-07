1d85395e885b945adb1791560cb6b5f5d55be251
Chaitanya Tangudu <chaitanya.tangudu@gmail.com>
2014-11-06 19:40:07 -0600

    Adding to repository

---
+++ MyJavaProj/src/my/java/MyExceptionHandling.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package my.java;
+
+/**
+ * @author Chaitanya
+ *
+ */
+public class MyExceptionHandling {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception{
+		try{
+			throw new Exception();
+		} finally{
+			
+		}
+
+	}
+
+}
