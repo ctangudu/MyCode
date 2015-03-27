1422f2e81305dc23f9cf044528fc26b8ed7313ac
Chaitanya Tangudu <chaitanya.tangudu@gmail.com>
2015-03-27 10:15:03 -0500

    adding

---
+++ MyJavaProj/src/my/java/MyJDBCClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package my.java;
+
+/**
+ * @author 929284
+ *
+ */
+public class MyJDBCClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
