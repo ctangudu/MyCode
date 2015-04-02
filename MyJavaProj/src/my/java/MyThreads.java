2c6ecdb6e7741a17275005702ff0e2a29b154a1e
Chaitanya Tangudu <chaitanya.tangudu@gmail.com>
2015-04-01 21:22:31 -0500

    Added changes

--- MyJavaProj/src/my/java/MyThreads.java
+++ MyJavaProj/src/my/java/MyThreads.java
@@ -1,27 +1,27 @@
 package my.java;
 
 public class MyThreads {
-	
+
 	public static void main(String[] args) {
 		Thread thread = new Thread(new MyRunnable());
 		thread.setName("Thread 1");
 		thread.start();
-//		thread = new Thread(new MyRunnable());
-//		thread.setName("Thread 2");
-//		thread.start();
-//		thread = new Thread(new MyRunnable());
-  		
+		// thread = new Thread(new MyRunnable());
+		// thread.setName("Thread 2");
+		// thread.start();
+		// thread = new Thread(new MyRunnable());
+
 	}
-	
 
 }
-class MyRunnable implements Runnable{
-	
+
+class MyRunnable implements Runnable {
+
 	@Override
 	public void run() {
-		for (int i = 0; i < 100 ; i++) {
-			if(i%10==0){
-				System.out.println("Modulus is "+0);
+		for (int i = 0; i < 100; i++) {
+			if (i % 10 == 0) {
+				System.out.println("Modulus is " + 0);
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -29,9 +29,9 @@ class MyRunnable implements Runnable{
 					e.printStackTrace();
 				}
 			}
-//			System.out.println("Run by "+Thread.currentThread().getName()+" Printing "+i);
+			// System.out.println("Run by "+Thread.currentThread().getName()+" Printing "+i);
 		}
-		
+
 	}
-	
+
 }
