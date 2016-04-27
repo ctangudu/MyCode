2077eb3ec6e33fb10540e42f7e365f02ec803535
Chaitanya Tangudu <chaitanya.tangudu@gmail.com>
2016-04-27 00:03:18 -0500

    added changes

--- SpringDemo/src/my/pack/DrawingApplication.java
+++ SpringDemo/src/my/pack/DrawingApplication.java
@@ -1,11 +1,7 @@
 package my.pack;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.web.context.support.XmlWebApplicationContext;
 
 public class DrawingApplication {
 	public static void main(String[] args) {
--- SpringDemo/src/my/pack/Triangle.java
+++ SpringDemo/src/my/pack/Triangle.java
@@ -1,18 +1,33 @@
 package my.pack;
 
 public class Triangle {
-	
-	private String type;
-	
-	public void draw() {
-		System.out.println(getType()+" Triangle drawing");
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	public Point getPointA() {
+		return pointA;
+	}
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+	public Point getPointB() {
+		return pointB;
+	}
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+	public Point getPointC() {
+		return pointC;
+	}
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
-	public String getType() {
-		return type;
+	public void draw(){
+		System.out.println("Point A "+pointA.getX()+" "+pointA.getY());
+		System.out.println("Point B "+pointB.getX()+" "+pointB.getY());
+		System.out.println("Point C "+pointC.getX()+" "+pointC.getY());
 	}
+	
 
-	public void setType(String type) {
-		this.type = type;
-	}
 }
